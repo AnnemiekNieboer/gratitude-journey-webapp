995c79a44dd8826613f27454e69ecfdad7c28263
AnnemiekNieboer <annemieknieboer@gmail.com>
2023-03-14 16:22:44 +0100

    start of navigation

---
+++ src/components/navigation/Navigation.tsx
@@ -0,0 +1,51 @@
+import React from 'react';
+import "./Navigation.css";
+import {NavLink} from "react-router-dom";
+import myJourneyIcon from "../../assets/icons/my-journey.svg";
+import boostIcon from "../../assets/icons/boost.svg";
+import addGratitudeIcon from "../../assets/icons/add-gratitude.svg";
+import travelLogIcon from "../../assets/icons/travel-log.svg";
+import accountIcon from "../../assets/icons/my-account.svg";
+
+const Navigation = () => {
+    return (
+        <div className="navigation__container">
+            <div className="navigation__inner-container">
+                <ul className="navigation__list">
+                    <li>
+                        <NavLink className="navigation__link" to="/my-journey">
+                            <img className="navigation__icon" src={myJourneyIcon} alt="My-Journey-icon"></img>
+                            My Journey
+                        </NavLink>
+                    </li>
+                    <li>
+                        <NavLink className="navigation__link" to="/boost">
+                            <img className="navigation__icon" src={boostIcon} alt="Boost-icon"></img>
+                            Boost
+                        </NavLink>
+                    </li>
+                    <li>
+                        <NavLink className="navigation__link" to="/add-gratitude">
+                            <img className="navigation__icon" src={addGratitudeIcon} alt="Add-gratitude-icon"></img>
+                            Add gratitude
+                        </NavLink>
+                    </li>
+                    <li>
+                        <NavLink className="navigation__link" to="/travel-log">
+                            <img className="navigation__icon" src={travelLogIcon} alt="Travel-log-icon"></img>
+                            Travel log
+                        </NavLink>
+                    </li>
+                    <li>
+                        <NavLink className="navigation__link" to="/my-account">
+                            <img className="navigation__icon" src={accountIcon} alt="My-Journey-icon"></img>
+                            Account
+                        </NavLink>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    );
+};
+
+export default Navigation;
--- src/pages/myJourney/MyJourney.tsx
+++ src/pages/myJourney/MyJourney.tsx
@@ -1,10 +1,15 @@
 import React from 'react';
 import "./MyJourney.css";
+import Logo from "../../components/logo/Logo";
+import Navigation from "../../components/navigation/Navigation";
 
 const MyJourney = () => {
     return (
-        <div>
-
+        <div className="outer-container__reusable">
+            <header className="inner-container__reusable">
+                <Logo logoClassName="logo--small"/>
+                <Navigation/>
+            </header>
         </div>
     );
 };
