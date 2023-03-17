bfe67bbbe5af929016999c58a638fb2c9f366c35
AnnemiekNieboer <annemieknieboer@gmail.com>
2023-03-17 13:34:57 +0100

    added pages

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,10 @@ import './App.css';
 import Homepage from "./pages/homepage/Homepage";
 import Login from "./pages/login/Login";
 import MyJourney from "./pages/myJourney/MyJourney";
+import Boost from "./pages/boost/Boost";
+import AddGratitude from "./pages/addGratitude/AddGratitude";
+import TravelLog from "./pages/travelLog/TravelLog";
+import Account from "./pages/account/Account";
 
 function App() {
     return (
@@ -15,6 +19,10 @@ function App() {
                 <Route path="/" element={<Homepage/>}/>
                 <Route path="/login" element={<Login/>}/>
                 <Route path="/my-journey" element={<MyJourney/>}/>
+                <Route path="/boost" element={<Boost/>}/>
+                <Route path="/add-gratitude" element={<AddGratitude/>}/>
+                <Route path="/travel-log" element={<TravelLog/>}/>
+                <Route path="/account" element={<Account/>}/>
             </Routes>
         </div>
     );
