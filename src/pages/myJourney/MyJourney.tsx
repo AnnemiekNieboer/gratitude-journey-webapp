542f510c0feaa6ab3295944c76e80da5aea9d6be
AnnemiekNieboer <annemieknieboer@gmail.com>
2023-03-17 17:26:22 +0100

    added first css art

--- src/pages/myJourney/MyJourney.tsx
+++ src/pages/myJourney/MyJourney.tsx
@@ -6,6 +6,17 @@ const MyJourney = () => {
     return (
         <div className="outer-container__reusable">
             <Header/>
+            <main className="inner-container__reusable my-journey__art-container--outer">
+                <div className="my-journey__art-container--inner">
+                    <div className="my-journey__art--items my-journey__art--item1"/>
+                    <div className="my-journey__art--items my-journey__art--item2"/>
+                    <div className="my-journey__art--items my-journey__art--item3"/>
+                    <div className="my-journey__art--items my-journey__art--item4"/>
+                    <div className="my-journey__art--items my-journey__art--item5"/>
+                    <div className="my-journey__art--items my-journey__art--item6"/>
+                    <div className="my-journey__art--items my-journey__art--item7"/>
+                </div>
+            </main>
         </div>
     );
 };
