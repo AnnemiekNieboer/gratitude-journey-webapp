678fe3fa887426dd659d943931a6076dd7c47481
AnnemiekNieboer <annemieknieboer@gmail.com>
2023-03-20 15:49:21 +0100

    added river and tree

--- src/pages/myJourney/MyJourney.tsx
+++ src/pages/myJourney/MyJourney.tsx
@@ -12,9 +12,40 @@ const MyJourney = () => {
                     <div className="my-journey__art--items my-journey__art--item2"/>
                     <div className="my-journey__art--items my-journey__art--item3"/>
                     <div className="my-journey__art--items my-journey__art--item4"/>
-                    <div className="my-journey__art--items my-journey__art--item5"/>
+                    <div className="my-journey__art--items my-journey__art--item5">
+                        <div className="item5__wave--container">
+                            <div className="item5__wave"></div>
+                            <div className="item5__wave item5__wave--2"></div>
+                        </div>
+                        <div className="item5__wave--container wave__container--2">
+                            <div className="item5__wave"/>
+                            <div className="item5__wave item5__wave--2"/>
+                        </div>
+                        <div className="item5__wave--container wave__container--3">
+                            <div className="item5__wave"/>
+                            <div className="item5__wave item5__wave--2"/>
+                        </div>
+                    </div>
                     <div className="my-journey__art--items my-journey__art--item6"/>
-                    <div className="my-journey__art--items my-journey__art--item7"/>
+                    <div className="my-journey__art--items my-journey__art--item7">
+                        <div className="item7__trees--container">
+                            <div className="item7__tree">
+                                <div className="item7__tree--greenery"/>
+                                <div className="item7__tree--greenery item7__tree--greenery--2"/>
+                                <div className="item7__tree--greenery item7__tree--greenery--3"/>
+                            </div>
+                            <div className="item7__tree item7__tree--2">
+                                <div className="item7__tree--greenery"/>
+                                <div className="item7__tree--greenery item7__tree--greenery--2"/>
+                                <div className="item7__tree--greenery item7__tree--greenery--3"/>
+                            </div>
+                            <div className="item7__tree item7__tree--3">
+                                <div className="item7__tree--greenery"/>
+                                <div className="item7__tree--greenery item7__tree--greenery--2"/>
+                                <div className="item7__tree--greenery item7__tree--greenery--3"/>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </main>
         </div>
